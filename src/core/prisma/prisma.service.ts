d81e1ae441007bac3d3d14ae6a04c0ef52ecf3f4
Kate <kate.dew.next@gmail.com>
2026-01-25 15:23:42 +0100

    setup sessions

---
+++ src/core/prisma/prisma.service.ts
@@ -0,0 +1,21 @@
+import {
+	Injectable,
+	type OnModuleDestroy,
+	type OnModuleInit
+} from '@nestjs/common'
+
+import { PrismaClient } from '@/prisma/generated/client'
+
+@Injectable()
+export class PrismaService
+	extends PrismaClient
+	implements OnModuleInit, OnModuleDestroy
+{
+	public async onModuleInit() {
+		await this.$connect()
+	}
+
+	public async onModuleDestroy() {
+		await this.$disconnect()
+	}
+}
