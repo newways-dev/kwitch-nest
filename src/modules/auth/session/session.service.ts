7f32f838323e13668b4813de9f334ab535cefbb0
Kate <kate.dew.next@gmail.com>
2026-01-26 13:50:35 +0100

    get and clear sessions

--- src/modules/auth/session/session.service.ts
+++ src/modules/auth/session/session.service.ts
@@ -23,6 +23,52 @@ export class SessionService {
 		private readonly configService: ConfigService
 	) {}
 
+	public async findByUser(req: Request) {
+		const userId = req.session.userId
+
+		if (!userId) {
+			throw new NotFoundException('Пользователь не обнаружен в сессии')
+		}
+
+		const keys = await this.redisService.keys('*')
+
+		const userSessions = []
+
+		for (const key of keys) {
+			const sessionData = await this.redisService.get(key)
+
+			if (sessionData) {
+				const session = JSON.parse(sessionData)
+
+				if (session.userId === userId) {
+					userSessions.push({
+						...session,
+						id: key.split(':')[1]
+					})
+				}
+			}
+		}
+
+		userSessions.sort((a, b) => b.createdAt - a.createdAt)
+
+		return userSessions.filter(session => session.id !== req.session.id)
+	}
+
+	public async findCurrent(req: Request) {
+		const sessionId = req.session.id
+
+		const sessionData = await this.redisService.get(
+			`${this.configService.getOrThrow<string>('SESSION_FOLDER')}${sessionId}`
+		)
+
+		const session = JSON.parse(sessionData)
+
+		return {
+			...session,
+			id: sessionId
+		}
+	}
+
 	public async login(req: Request, input: LoginInput, userAgent: string) {
 		const { login, password, pin } = input
 
@@ -80,5 +126,25 @@ export class SessionService {
 				resolve(true)
 			})
 		})
+	}
+
+	public async clearSession(req: Request) {
+		req.res.clearCookie(
+			this.configService.getOrThrow<string>('SESSION_NAME')
+		)
+
+		return true
+	}
+
+	public async remove(req: Request, id: string) {
+		if (req.session.id === id) {
+			throw new ConflictException('Текущую сессию удалить нельзя')
+		}
+
+		await this.redisService.del(
+			`${this.configService.getOrThrow<string>('SESSION_FOLDER')}${id}`
+		)
+
+		return true
 	}
 }
